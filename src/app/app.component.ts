8e4068d99fbade092903d1636366002695d718e8
EmersonCarlosAlvesSouza <emersoncarlosalves21@gmail.com>
2024-06-26 11:59:40 -0300

    Criando Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports:  [MeuPrimeiroComponent, MeuPrimeiro2Component],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'primeiro-projeto';
